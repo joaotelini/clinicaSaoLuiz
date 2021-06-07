6720d4299a6ab8f0753621bae9f0d0e114f14c08
Joao Pedro <jobastico0987@gmail.com>
2021-06-07 15:06:49 -0300

    criando o arquivo conexao.php na pasta controller

---
+++ clinica/Controller/conexao.php
@@ -0,0 +1,22 @@
+<?php
+#Controller\conexao.php
+class Conexao{
+	private static $instance;
+	
+	private function __construct() {
+        //
+    }
+	public static function getInstance(){
+		if (!isset(self::$instance)){
+			self::$instance = new PDO('mysql:host=localhost; dbname=clinica', 'root', '',
+			array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+			
+			self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+			self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+		}
+		
+		return self::$instance;
+	}
+}
+?>
