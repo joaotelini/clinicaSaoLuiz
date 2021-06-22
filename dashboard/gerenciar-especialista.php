5913f05245038eca32874f3b8272f9a68a99d08c
João Pedro Domingues Telini <58189906+joaotelini@users.noreply.github.com>
2021-06-22 09:17:43 -0300

    Add files via upload

---
+++ dashboard/gerenciar-especialista.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Tables</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-text mx-3">Clinica São Luiz</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Página Inicial</span></a>
+            </li>
+
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Nav Item - Tables -->
+            <li class="nav-item">
+                <a class="nav-link" href="agendamentos.html">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Agendamentos</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="departamentos.html">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Departamentos</span></a>
+            </li>
+
+            <li class="nav-item active">
+                <a class="nav-link" href="especialistas.html">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Especialistas</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <form class="form-inline">
+                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                    </form>
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Administrador(a)</span>
+                                <img class="assets/img-profile rounded-circle"
+                                    src="assets/img/undraw_profile.svg">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Sair
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Gerenciar os Especialistas</h1>
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Profissional</th>
+                                            <th>Especialidade</th>
+                                            <th>Email</th>
+                                            <th>Telefone</th>
+                                            <th>Ações</th>                                           
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Profissional</th>
+                                            <th>Especialidade</th>
+                                            <th>Email</th>
+                                            <th>Telefone</th>
+                                            <th>Ações</th>       
+                                        </tr>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <tr>
+                                            <td>Raimunda Giovanna Rafaela dos Santos</td>
+                                            <td>Cardiologia</td>
+                                            <td>raimundagiovannarafaeladossantos-92@unink.com.br</td>
+                                            <td>(19) 98223-2027</td>
+                                            <td>Editar, Apagar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Raimunda Giovanna Rafaela dos Santos</td>
+                                            <td>Cardiologia</td>
+                                            <td>raimundagiovannarafaeladossantos-92@unink.com.br</td>
+                                            <td>(19) 98223-2027</td>
+                                            <td>Editar, Apagar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Raimunda Giovanna Rafaela dos Santos</td>
+                                            <td>Cardiologia</td>
+                                            <td>raimundagiovannarafaeladossantos-92@unink.com.br</td>
+                                            <td>(19) 98223-2027</td>
+                                            <td>Editar, Apagar</td>                                         
+                                            
+                                        </tr>
+                                        <tr>
+                                            <td>Raimunda Giovanna Rafaela dos Santos</td>
+                                            <td>Cardiologia</td>
+                                            <td>raimundagiovannarafaeladossantos-92@unink.com.br</td>
+                                            <td>(19) 98223-2027</td>
+                                            <td>Editar, Apagar</td>
+                                        </tr>
+                                        <td>Raimunda Giovanna Rafaela dos Santos</td>
+                                            <td>Cardiologia</td>
+                                            <td>raimundagiovannarafaeladossantos-92@unink.com.br</td>
+                                            <td>(19) 98223-2027</td>
+                                            <td>Editar, Apagar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Raimunda Giovanna Rafaela dos Santos</td>
+                                            <td>Cardiologia</td>
+                                            <td>raimundagiovannarafaeladossantos-92@unink.com.br</td>
+                                            <td>(19) 98223-2027</td>
+                                            <td>Editar, Apagar</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Raimunda Giovanna Rafaela dos Santos</td>
+                                            <td>Cardiologia</td>
+                                            <td>raimundagiovannarafaeladossantos-92@unink.com.br</td>
+                                            <td>(19) 98223-2027</td>
+                                            <td>Editar, Apagar</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Deseja sair?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Clique em "Sair" para encerrar a sesssão.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                    <a class="btn btn-primary" href="login.html">Sair</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="assets/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="assets/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="assets/js/demo/datatables-demo.js"></script>
+
+</body>
+
+</html>
