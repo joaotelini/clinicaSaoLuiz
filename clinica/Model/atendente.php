9845ca474a5faf0a0c012ddd2410b0d074bda071
João Pedro <jobastico0987@gmail.com>
2021-07-23 18:40:13 -0300

    criando a class atendente

---
+++ clinica/Model/atendente.php
@@ -0,0 +1,63 @@
+<?php
+
+    class Atendente {
+
+        private $idAtendente;
+        private $nome;
+        private $cpf;
+        private $email;
+        private $senha;
+
+        public function __construct($idA, $n, $cpf, $e, $s) {
+
+            $this->idAtendente = $idA;
+            $this->nome = $n;
+            $this->cpf = $cpf;
+            $this->email = $e;
+            $this->senha = $s;
+
+        }
+
+        public function getIdAtendente() {
+            return $this->idAtendente;
+        }
+
+        public function setIdAtendente($idA) {
+            $this->idAtendente = $idA;
+        }
+
+        public function getNome() {
+            return $this->nome;
+        }
+
+        public function setNome($n) {
+            $this->nome = $n;
+        }
+
+        public function getCpf() {
+            return $this->cpf;
+        }
+
+        public function setCpf($cpf) {
+            $this->cpf = $cpf;
+        }
+
+        public function getEmail() {
+            return $this->email;
+        }
+
+        public function setEmail($e) {
+            $this->email = $e;
+        }
+
+        public function getSenha() {
+            return $this->senha;
+        }
+
+        public function setSenha($s) {
+            $this->senha = $s;
+        }
+
+    }
+
+?>
