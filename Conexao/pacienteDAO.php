b5bf2928b38406a8968081c5417fdc170cf5b6c2
= <jobastico0987@gmail.com>
2021-08-12 09:09:05 -0300

    organizando arquivo pacientePadastrar.php e a classe paciente.php

--- Conexao/pacienteDAO.php
+++ Conexao/pacienteDAO.php
@@ -6,24 +6,9 @@
         public function Inserir(Paciente $pac) {
             try{
 
-                if ((!empty($pac->getNome())) && (!empty($pac->getCpf())) && (!empty($pac->getEmail())) && (!empty($pac->getSenha())) && (!empty($pac->getTelefone())) && (!empty($pac->getRg())) && (!empty($pac->getDataNascimento())) && (!empty($pac->getLogradouro())) && (!empty($pac->getCidade())) && (!empty($pac->getBairro())) && (!empty($pac->getEstado())) && (!empty($pac->getCep()))) {
-                    
-					$sql = "INSERT INTO paciente VALUES (default, :nome, :cpf, :email, :senha, :telefone, :rg, :dataNascimento, :logradouro, :cidade, :bairro, :estado, :cep)";
-					
-					$sp_sql = Conexao::getInstance()->prepare($sql);
-					
-					$sp_sql->bindValue(":nome", $pac->getNome());
-					$sp_sql->bindValue(":cpf", $pac->getCpf());
-                    $sp_sql->bindValue(":email", $pac->getEmail());
-					$sp_sql->bindValue(":senha", md5($pac->getSenha()));
-                    $sp_sql->bindValue(":telefone", $pac->getTelefone());
-                    $sp_sql->bindValue(":rg", $pac->getRg());
-                    $sp_sql->bindValue(":dataNascimento", $pac->getDataNascimento());
-                    $sp_sql->bindValue(":logradouro", $pac->getLogradouro());
-                    $sp_sql->bindValue(":cidade", $pac->getCidade());
-                    $sp_sql->bindValue(":bairro", $pac->getBairro());
-                    $sp_sql->bindValue(":estado", $pac->getEstado());
-                    $sp_sql->bindValue(":cep", $pac->getCep());
+
+
+                $pdo = Conexao::getIntance();
 					
 					return $sp_sql->execute();
 				}
--- Controller/pacienteCadastrar.php
+++ Controller/pacienteCadastrar.php
@@ -11,14 +11,12 @@
 	$rg = $_POST['rg'];
 	$dataNascimento = $_POST['dataNascimento'];
 	$logradouro = $_POST['logradouro'];	
-	$cidade = $_POST['cidade'];
-	$bairro = $_POST['bairro'];
-	$estado = $_POST['estado'];
+	$numero = $_POST['numero'];
 	$cep = $_POST['cep'];
 	
 	
 	//Instanciar o objeto
-	$pac = new Paciente($nome, $cpf, $email, $senha, $telefone, $rg, $dataNascimento, $logradouro, $cidade, $bairro, $estado, $cep);
+	$pac = new Paciente($nome, $cpf, $email, $senha, $telefone, $rg, $dataNascimento, $logradouro, $numero, $cep);
 	$pacDAO = new PacienteDAO();
 	
 	//Chamar o método
